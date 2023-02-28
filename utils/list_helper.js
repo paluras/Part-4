51ff139e4dbdb827a3d6d600fc95bb66f4045737
Palura Andrei <98059798+paluras@users.noreply.github.com>
2023-02-28 15:47:34 +0200

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy =(blogs) =>{
     return 1
 }
 
+const totalLikes = (blogsLikes) => {
+    return blogsLikes.map(obj => obj.likes).reduce((a, b) => a + b, 0)
+
+}
+
+
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
