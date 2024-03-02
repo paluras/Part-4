c30839a39bc3b51e96ce3542f23ee397b1951619
Palura Andrei <98059798+paluras@users.noreply.github.com>
2024-03-03 00:28:29 +0200

    4.11 Completed

--- tests/listBack.test.js
+++ tests/listBack.test.js
@@ -1,26 +1,110 @@
-const { test, after } = require('node:test')
-const  assert  = require('node:assert')
+const { test, after, beforeEach } = require('node:test')
+const Blog = require('../models/blog')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 
-    test('blogs return json' , async () => {
-        await api
-            .get('/api/blogs')
-            .expect(200)
-            .expect('Content-Type', /application\/json/)
-    })
+const initialBlog = [
+    {
 
-    test('blogs list amount', async () => {
+        "title": "Harry Potter",
+        "author": "Jim Carrey",
+        "url": "text",
+        "likes": 10
+        ,
+        "important": false
+    }, {
 
-        const response = await api.get('/api/blogs')
+        "title": "Barrsss Potter",
+        "author": " Carrey",
+        "url": "textestt",
+        "likes": 2323
+        ,
+        "important": true
+    }
+]
 
-        assert.strictEqual(response.body.length, 1)
-     
-    })
 
-    after(async () => {
-        await mongoose.connection.close()
-    })
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObj = new Blog(initialBlog[0])
+    await blogObj.save()
+    blogObj = new Blog(initialBlog[1])
+    await blogObj.save()
+})
+
+test('blogs return json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+test('blogs list amount', async () => {
+
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, 2)
+
+})
+
+test('first object returns "id" key', async () => {
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body[0].hasOwnProperty('id'), true)
+})
+
+test('valid blog is added', async () => {
+
+    const validBlog =
+    {
+
+        "title": "Barrsss Potter",
+        "author": " Carrey",
+        "url": "textestt",
+        "likes": 2323233
+        ,
+        "important": true
+    }
+
+
+    await api
+        .post('/api/blogs')
+        .send(validBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, initialBlog.length + 1)
+})
+
+test('blog is missing likes', async () => {
+
+    const noLikesBlog =
+    {
+
+        "title": "Barrsss Potter",
+        "author": " Carre23333y",
+        "url": "textestt",
+
+
+        "important": true
+    }
+
+
+    await api
+        .post('/api/blogs')
+        .send(noLikesBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    console.log(response.body, "my log");
+    assert.strictEqual(response.body[response.body.length - 1].likes, 0)
+})
+
+after(async () => {
+    await mongoose.connection.close()
+})
 
